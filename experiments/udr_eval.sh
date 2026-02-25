ae70f4b57a21ffd8b11e9f7cf3c3fb5bfe416b22
sukchul <tjrcjf410@snu.ac.kr>
2026-02-25 14:43:37 +0900

    ASef

--- experiments/udr_eval.sh
+++ experiments/udr_eval.sh
@@ -1,5 +1,5 @@
 wandb_project=factory-peginsert
 for seed in 0 1 2 3 4 5
 do
-    python scripts/rl_games/evaluate_async.py --task Custom-Factory-PegInsert-Direct-UDR-v0 --headless --track --seed $seed --wandb-project-name $wandb_project
+    python scripts/rl_games/evaluate_async.py --task Custom-Factory-PegInsert-Direct-UDR-v0 --mode all --num_envs 1024 --eval_episodes 10 --headless --track --seed $seed --wandb-project-name $wandb_project
 done 
